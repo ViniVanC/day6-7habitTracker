83c6c538153ae3f370464ad286673737295c89bb
ViniVanC <vincentvan0302@gmail.com>
2023-03-03 13:11:53 +0200

    added mapping of HabitItem to HabitList

--- src/components/Main/Habits/HabitList.jsx
+++ src/components/Main/Habits/HabitList.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useHabits } from "../../../hooks/useHabits";
 import { HabitItem } from "./HabitItem";
 
 export const HabitList = () => {
+  const { habitsList } = useHabits();
+
   return (
     <ul>
-      <HabitItem />
+      {habitsList.map((item) => (
+        <HabitItem
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          description={item.description}
+          allDays={item.allDays}
+          currentDays={item.currentDays}
+          check={item.check}
+        />
+      ))}
     </ul>
   );
 };
