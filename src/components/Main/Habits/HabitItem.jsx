5bc49c9f7643d0e9abf2d6038d113cacf1ab4e7b
ViniVanC <vincentvan0302@gmail.com>
2023-03-03 13:09:13 +0200

    added data to component HabitItem

--- src/components/Main/Habits/HabitItem.jsx
+++ src/components/Main/Habits/HabitItem.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
 import { FaCheck } from "react-icons/fa";
+import { useHabits } from "../../../hooks/useHabits";
 
-export const HabitItem = ({ id, title }) => {
+export const HabitItem = ({
+  id,
+  title,
+  description,
+  allDays,
+  currentDays,
+  check,
+}) => {
+  const { handleCheck } = useHabits();
+
   return (
-    <div className="relative min-h-[200px] max-w-[200px] bg-sandy-brown text-blue-dianne p-[30px] pt-[20px] rounded-[15px] shadow-lg">
+    <div className="relative min-h-[200px] max-w-[200px]  bg-sandy-brown text-blue-dianne p-[30px] pt-[20px] rounded-[15px] shadow-lg">
       <h3 className="mb-[20px] text-[20px]">{title}</h3>
       {/* progress bar */}
-      <CircularProgressbar value={50} text={`1/30`} />
+      <CircularProgressbar value={50} text={`${currentDays}/${allDays}`} />
       <button
-        className={
-          "absolute top-[20px] right-[20px] w-[30px] h-[30px] border-[4px] border-solid rounded-[50%]"
-        }
-      ></button>
+        className={`absolute top-[20px] right-[20px] w-[30px] h-[30px] border-[4px] border-solid rounded-[50%] transition duration-300 flex items-center justify-center ${
+          check ? "bg-blue-dianne" : ""
+        }`}
+        onClick={() => handleCheck(id)}
+      >
+        {check && <FaCheck className="text-sandy-brown" />}
+      </button>
     </div>
   );
 };
