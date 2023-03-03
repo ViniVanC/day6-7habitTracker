2f02e40354fe9268c1336411cbacdaa86a1b1b31
ViniVanC <vincentvan0302@gmail.com>
2023-03-03 13:07:07 +0200

    added logic press at check button

--- src/hooks/useHabits.jsx
+++ src/hooks/useHabits.jsx
@@ -1,8 +1,36 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
+import { v4 } from "uuid";
 
 const HabitsContext = createContext();
 export const useHabits = () => useContext(HabitsContext);
 
 export const HabitsProvider = ({ children }) => {
-  return <HabitsContext.Provider value={{}}>{children}</HabitsContext.Provider>;
+  const [habitsList, setHabitsList] = useState([
+    {
+      id: v4(),
+      title: "title",
+      description: "",
+      allDays: 30,
+      currentDays: 1,
+      check: false,
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    setHabitsList(
+      habitsList.map((item) =>
+        item.id === id ? { ...item, check: !item.check } : item
+      )
+    );
+  };
+
+  const deleteHabitsItem = (id) => {};
+
+  return (
+    <HabitsContext.Provider
+      value={{ habitsList, handleCheck, deleteHabitsItem }}
+    >
+      {children}
+    </HabitsContext.Provider>
+  );
 };
