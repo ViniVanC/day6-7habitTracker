7678e5d011c69f39a0443ea65f8c61bf61773033
ViniVanC <vincentvan0302@gmail.com>
2023-03-03 10:41:48 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
 
 function App() {
   return (
-    <div className="h-[100vh] flex items-center justify-center text-[400px] font-black">
-      hi
+    <div className="min-h-[100vh] flex flex-col bg-blue-dianne text-burnt-sienna">
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
