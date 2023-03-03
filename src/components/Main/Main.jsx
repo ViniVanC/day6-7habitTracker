52a9c1c5e7d2aa9279b9c665b592ad7de177cd91
ViniVanC <vincentvan0302@gmail.com>
2023-03-03 10:41:28 +0200

    created component Main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "../Container";
+
+export const Main = () => {
+  return (
+    <main className="mt-[90px] flex-grow flex-shrink basis-auto">
+      <Container>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo
+        repudiandae ullam ut officia facilis, harum officiis earum iusto aut
+        deleniti numquam corporis? Distinctio omnis enim blanditiis error odit
+        optio repudiandae.
+      </Container>
+    </main>
+  );
+};
