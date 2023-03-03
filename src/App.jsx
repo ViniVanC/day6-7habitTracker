a3e1a517f2ce2b6bb23d6fbd91cbf13d9a4df2b4
ViniVanC <vincentvan0302@gmail.com>
2023-03-03 09:51:57 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="h-[100vh] flex items-center justify-center text-[400px] font-black">
-      hi{" "}
+      hi
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-dianne": "#264653",
+        "jungle-green": "#2A9D8F",
+        "rob-roy": "#E9C46A",
+        "sandy-brown": "#F4A261",
+        "burnt-sienna": "#E76F51",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
 };
