26da1b1b154e6cdbb75aa5908b42ce0cb54ad7c4
ViniVanC <vincentvan0302@gmail.com>
2023-03-03 12:08:59 +0200

    commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Container } from "../Container";
+import { HabitList } from "./Habits/HabitList";
 
 export const Main = () => {
   return (
     <main className="mt-[90px] flex-grow flex-shrink basis-auto">
       <Container>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo
-        repudiandae ullam ut officia facilis, harum officiis earum iusto aut
-        deleniti numquam corporis? Distinctio omnis enim blanditiis error odit
-        optio repudiandae.
+        <div className="">
+          <HabitList />
+        </div>
       </Container>
     </main>
   );
