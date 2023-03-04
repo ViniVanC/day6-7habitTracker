324d997cd1c81eff554c686362adbb24a759414a
ViniVanC <vincentvan0302@gmail.com>
2023-03-04 14:10:05 +0200

    added logic createHabitItem and created component createHabitItem

---
+++ src/components/Main/Habits/CreateHabitItem.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { useHabits } from "../../../hooks/useHabits";
+import { useVars } from "../../../hooks/useVars";
+import { HiXMark } from "react-icons/hi2";
+
+export const CreateHabitItem = () => {
+  const { createHabitsItem, setOpenCreateHabitsItemBubble } = useHabits();
+  const { newHabitItem, setNewHabitItem } = useVars();
+  return (
+    <>
+      <div
+        className="absolute top-0 left-0 z-[60] bg-black/30 w-[100vw] h-[100vh] backdrop-blur-md"
+        onClick={setOpenCreateHabitsItemBubble}
+      />
+      <form
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[60] flex flex-col  bg-blue-dianne p-[30px] rounded-[15px] shadow-lg"
+        onSubmit={(f) => f}
+      >
+        <button
+          className="absolute top-[5px] right-[5px] text-[30px] transition duration-300 hover:scale-110 active:scale-90"
+          onClick={setOpenCreateHabitsItemBubble}
+        >
+          <HiXMark />
+        </button>
+        <input
+          className="bg-transparent text-[30px] font-bold mb-[10px] text-burnt-sienna placeholder:text-burnt-sienna"
+          type="text"
+          placeholder="Title"
+          value={newHabitItem.title}
+          onChange={(e) =>
+            setNewHabitItem({ ...newHabitItem, title: e.target.value })
+          }
+        />
+        <textarea
+          className="bg-transparent text-burnt-sienna placeholder:text-burnt-sienna resize-none"
+          cols="50"
+          rows="10"
+          placeholder="Description..."
+          value={newHabitItem.description}
+          onChange={(e) =>
+            setNewHabitItem({ ...newHabitItem, description: e.target.value })
+          }
+        />
+        <div className="mb-[20px] text-right">
+          <label className="font-semibold">{newHabitItem.allDays}</label>
+          <input
+            type="range"
+            min={7}
+            max={90}
+            step={1}
+            value={newHabitItem.allDays}
+            onChange={(e) =>
+              setNewHabitItem({ ...newHabitItem, allDays: e.target.value })
+            }
+          />
+        </div>
+        <button
+          className="p-[10px] text-blue-dianne bg-burnt-sienna font-bold rounded-[10px] transition duration-300 hover:scale-105 active:scale-90"
+          onClick={createHabitsItem}
+        >
+          Add
+        </button>
+      </form>
+    </>
+  );
+};
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import { FaPlus } from "react-icons/fa";
 import { useHabits } from "../../hooks/useHabits";
 import { Container } from "../Container";
+import { CreateHabitItem } from "./Habits/CreateHabitItem";
 import { HabitList } from "./Habits/HabitList";
 
 export const Main = () => {
-  const { createHabitsItem } = useHabits();
+  const {
+    createHabitsItem,
+    openCreateHabitsItemBubble,
+    setOpenCreateHabitsItemBubble,
+  } = useHabits();
 
   return (
     <main className="mt-[90px] flex-grow flex-shrink basis-auto">
@@ -15,13 +20,14 @@ export const Main = () => {
             <h1 className="text-[30px] font-bold">Habit Tracker</h1>
             <button
               className="w-[50px] h-[50px] flex items-center justify-center text-[30px] border-[3px] border-solid rounded-[50%] transition duration-300 hover:scale-110 active:scale-90"
-              onClick={createHabitsItem}
+              onClick={setOpenCreateHabitsItemBubble}
             >
               <FaPlus />
             </button>
           </div>
           <hr />
           <HabitList />
+          {openCreateHabitsItemBubble ? <CreateHabitItem /> : ""}
         </div>
       </Container>
     </main>
