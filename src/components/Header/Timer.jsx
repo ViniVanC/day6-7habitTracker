38a0e1ada5235f88dca8f4684ded6873e64f1ac2
ViniVanC <vincentvan0302@gmail.com>
2023-03-04 18:29:40 +0200

    commit

--- src/components/Header/Timer.jsx
+++ src/components/Header/Timer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { FaMoon } from "react-icons/fa";
 import { useVars } from "../../hooks/useVars";
 
 export const Timer = () => {
   const { countdown } = useVars();
   return (
-    <div className="text-[20px] font-bold">
-      {countdown.hours}:{countdown.minutes}:{countdown.seconds}
+    <div className="flex items-center gap-[10px] text-[20px] font-bold">
+      <FaMoon /> {countdown.hours}:{countdown.minutes}:{countdown.seconds}
     </div>
   );
 };
