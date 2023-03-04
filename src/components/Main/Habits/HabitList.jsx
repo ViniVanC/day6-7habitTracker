df8d0009a6ef5dd60a687996f93c246777e9b7fe
ViniVanC <vincentvan0302@gmail.com>
2023-03-04 17:37:32 +0200

    bug-fix

--- src/components/Main/Habits/HabitList.jsx
+++ src/components/Main/Habits/HabitList.jsx
@@ -7,7 +7,7 @@ export const HabitList = () => {
 
   return (
     <ul
-      className={`flex max-[768px]:justify-center gap-[20px] flex-wrap ${
+      className={`flex justify-end max-[768px]:justify-center gap-[20px] flex-wrap ${
         grid ? "flex-row-reverse" : "flex-col-reverse"
       }`}
     >
